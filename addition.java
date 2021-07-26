58c52c37f5adae5d37d5db4afc41ec7299474d4f
Sruthyk-git <sruthy.kandange@ust.com>
2021-07-26 14:00:54 +0530

    new repo

---
+++ addition.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author ustjavab221
+ *
+ */
+public class addition {
+
+	public static void main(String[] args) {
+		int a=6;
+		int b=10;
+		int c=a+b;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
